549964ac183c6e2939bd06b409a9965e5382533f
Leopoldo Chavarria <leopoldomisaelchavarria@gmail.com>
2024-07-13 19:53:57 -0600

    Cambio

--- CalculadoraInteractica.js
+++ CalculadoraInteractica.js
@@ -1,13 +1,16 @@
-let pantalla = document.getElementById('pantalla');
-let numeroActual = '';
-let numeroAnterior = '';
-let operacion = null;
+/*DECLARACIÓN E INICIALIZACIÓN DE VARIABLES Y DOM */
+let pantalla = document.getElementById('pantalla')
+let numeroActual = ''
+let numeroAnterior = ''
+let operacion = null
 
+/*FUNCION PARA AGREGAR NUMEROS EN LA PANTALLA CUANDO SE HACE CLICK EN LOS BOTONES*/
 function agregarNumero(numero) {
     numeroActual += numero;
     pantalla.value = numeroActual;
 }
 
+/*FUNCION PARA CUANDO SE HACE CLICK EN LOS BOTONES DE OPERACIÓN */
 function establecerOperacion(op) {
     if (numeroActual === '') return;
     if (numeroAnterior !== '') {
@@ -18,11 +21,15 @@ function establecerOperacion(op) {
     numeroActual = '';
 }
 
+/*FUNCION QUE REALIZA LAS OPERACIONES DE LOS NÚMEROS ANTERIOR Y ACTUAL CUANDO APRETAMOS EL 
+BOTON DE '=', DEPENDIENDO EL SWITCH CASE CON LA VARIABLE OPERACIÓN EN DONDE SE GUARDA 
+LA OP QUE APRETAMOS, CUANDO TERMINA LA OPERCIÓN VUELVE A REASIGNAR EL NÚMERO ANTERIOR Y NUMERO 
+ACTUAL PARA ASÍ VOLVER A HACER INFINIDAD DE OPERACINES, MUESTRA EN LA PANTALLA EL RESULTADO*/
 function calcular() {
     if (numeroActual === '' || numeroAnterior === '') return;
     let resultado;
-    const anterior = parseFloat(numeroAnterior);
-    const actual = parseFloat(numeroActual);
+    const anterior = parseFloat(numeroAnterior); //CONVIERTE LOS STRING A FLOATS
+    const actual = parseFloat(numeroActual); //CONVIERTE LOS STRINGS A FLOATS
     switch (operacion) {
         case '+':
             resultado = anterior + actual;
@@ -45,6 +52,8 @@ function calcular() {
     pantalla.value = numeroActual;
 }
 
+/* REASIGNA LOS VALORES DE NÚMERO ANTERIOR, NÚMERO ACTUAL Y OPERACIÓN, PARA ASÍ VOLVER A 
+ESCOGER LOS VALORES A OPERAR JUNTO CON LA OPERACIÓN A REALIZAR*/
 function limpiarPantalla() {
     numeroActual = '';
     numeroAnterior = '';
